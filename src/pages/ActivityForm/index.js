b5d0f1320ee662dabe03e06b349559f3c1268f06
Bruno Garcia Rana <brunogarciarana@gmail.com>
2020-05-23 23:00:54 -0300

    form creating activity

--- src/pages/ActivityForm/index.js
+++ src/pages/ActivityForm/index.js
@@ -14,9 +14,27 @@ import {types, useAlert} from "react-alert";
 function ActivityForm() {
     const alert = useAlert();
     const [subjects, setSubjects] = useState({options: []});
+    const [title, setTitle] = useState('');
+    const [subjectKey, setSubjectKey] = useState('');
+    const [question, setQuestion] = useState('');
+    const [images, setImages] = useState([]);
+    const {getRootProps, getInputProps} = useDropzone({
+        accept: 'image/*',
+        maxSize: 4000000,
+        onDrop: acceptedFiles => {
+            const newFiles = acceptedFiles.map(file => Object.assign(file, {
+                preview: URL.createObjectURL(file),
+                imageId: generateId(8)
+            }));
+            const newFilesList = images.concat(newFiles);
+            setImages(newFilesList);
+        },
+        onDropRejected(fileRejections, event) {
+            alert("Não foi possivel fazer upload desta imagem. \n Tamanho máximo de 4mb");
+        }
+    });
 
     useEffect(() => {
-
         async function fetchData() {
             try {
                 const result = await api.get('/subjects');
@@ -29,30 +47,12 @@ function ActivityForm() {
         }
         fetchData();
     }, []);
+    useEffect(() => () => {
+        // Make sure to revoke the data uris to avoid memory leaks
+        images.forEach(file => URL.revokeObjectURL(file.preview));
+    },  [images]);
 
-    const [files, setFiles] = useState([]);
-    const {getRootProps, getInputProps} = useDropzone({
-        accept: 'image/*',
-        maxSize: 4000000,
-        onDrop: acceptedFiles => {
-            const newFiles = acceptedFiles.map(file => Object.assign(file, {
-                preview: URL.createObjectURL(file),
-                imageId: generateId(8)
-            }));
-            const newFilesList = files.concat(newFiles);
-            setFiles(newFilesList);
-        },
-        onDropRejected(fileRejections, event) {
-            alert("Não foi possivel fazer upload desta imagem. \n Tamanho máximo de 4mb");
-        }
-    });
-
-    function removeImage(imageId) {
-        const newFilesList = files.filter(elem => elem.imageId !== imageId);
-        setFiles(newFilesList);
-    }
-
-    const thumbs = files.map(file => {
+    const thumbs = images.map(file => {
         return (
             <div className='thumb' key={file.imageId} onClick={() => removeImage(file.imageId)}>
                 <div className='thumbInner'>
@@ -66,24 +66,45 @@ function ActivityForm() {
         )
     });
 
-    useEffect(() => () => {
-        // Make sure to revoke the data uris to avoid memory leaks
-        files.forEach(file => URL.revokeObjectURL(file.preview));
-    },  [files]);
+    function removeImage(imageId) {
+        const newFilesList = images.filter(elem => elem.imageId !== imageId);
+        setImages(newFilesList);
+    }
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        const formData = new FormData();
+
+        formData.append('title', title);
+        formData.append('subjectKey', parseInt(subjectKey));
+        formData.append('question', question);
+        images.forEach((image) => {
+            formData.append('images', image);
+        });
+
+        const response = await api.post('/activities', formData);
+    }
 
     return (
         <>
             <h1 className="title">Envio de atividade</h1>
-            <form action="">
+            <form onSubmit={handleSubmit} encType="multipart/form-data">
                 <div className="input-group">
                     <span>Titulo da atividade</span>
-                    <InputText name="title" placeholder="Digite aqui o seu título..." />
+                    <InputText name="title"
+                               value={title}
+                               onChange={e => setTitle(e.target.value)}
+                               placeholder="Digite aqui o seu título..." />
                 </div>
                 <div className="input-group">
                     <span>Selecione um assunto</span>
                     <div className="matter-container">
-                        <select name="matter" id="matter" className="matter">
-                            <option>Selecione um assunto</option>
+                        <select name="matter"
+                                id="matter"
+                                value={subjectKey}
+                                onChange={e => setSubjectKey(e.target.value)}
+                                className="matter">
+                            <option value={0}>Selecione um assunto</option>
                             {subjects.options.map((option) => (
                                 <option key={option.key} value={option.key}>{option.name}</option>
                             ))}
@@ -92,7 +113,10 @@ function ActivityForm() {
                 </div>
                 <div className="input-group">
                     <span>Digite aqui comentários sobre a sua dúvida</span>
-                    <TextArea name="comment" placeholder="Digite aqui o seu comentário..." />
+                    <TextArea name="comment"
+                              value={question}
+                              onChange={e => setQuestion(e.target.value)}
+                              placeholder="Digite aqui o seu comentário..." />
                 </div>
                 <div className="input-group dropzone-container">
                     <span>Caso queira, você também pode anexar fotos</span>
@@ -104,7 +128,7 @@ function ActivityForm() {
                 <div className="input-group">
                     <span>Fotos anexadas</span>
                     <div className='thumbsContainer'>
-                        {files.length === 0 ? (
+                        {images.length === 0 ? (
                             <div className='no-content'>
                                 <CloudOutlinedIcon className='cloud-icon' />
                                 <p>Nenhuma foto foi anexada ainda</p>
